c1054080246fe0aee06ed8a8f07d5a8b58ee3dee
mdabdurrahman07 <jamilrahman2001@gmail.com>
2026-05-02 23:37:06 +0600

    Add strict type constraints for student objects in TypeScript

--- constraint.ts
+++ constraint.ts
@@ -1,2 +1,27 @@
 // constraint:- strict rules
 
+type Students = {
+  id: number;
+  name: string;
+};
+
+const addStudentToCourse = <T extends Students>(studentInfo: T) => {
+  return {
+    course: "Next Level",
+    ...studentInfo,
+  };
+};
+
+const student1 = {
+  name: "Jamil",
+  id: 1,
+  class: 10,
+};
+const student2 = {
+  name: "Jamil",
+  class: 10,
+  id: 3,
+};
+
+const res1 = addStudentToCourse(student1);
+const res2 = addStudentToCourse(student2);
